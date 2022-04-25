893d8619abf1ad54781ec2d3907a3c0fa06f5b2d
dev.k <dev.krecord@gmail.com>
2022-04-25 20:54:16 +0900

    feat: Slider css 작업중

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -9,11 +9,15 @@ function Slide({ sliders }) {
     <Container>
       <SlideTitle>일별 박스오피스</SlideTitle>
       <StyledSlider {...settings}>
-        {sliders.map(({ name, image }, index) => {
+        {sliders.map(({ ranking, title, releaseDate, image }, index) => {
           return (
             <CardBox key={index}>
-              <CardImg src={image}></CardImg>
-              <CardText>{name}</CardText>
+              <CardImgBox>
+                <CardRanking>{ranking}</CardRanking>
+                <CardImg src={image}></CardImg>
+              </CardImgBox>
+              <CardTitle>{title}</CardTitle>
+              <CardText>개봉 : {releaseDate}</CardText>
             </CardBox>
           );
         })}
@@ -28,12 +32,13 @@ export default Slide;
 const settings = {
   dots: false, // 슬라이드 밑에 점 보이게
   infinite: true, // 무한으로 반복
-  speed: 500,
   autoplay: true,
+  initialSlide: 0,
+  centerMode: false,
+  speed: 500,
   autoplaySpeed: 2000, // 넘어가는 속도
-  slidesToShow: 4, // 4장씩 보이게
+  slidesToShow: 5, // 5장씩 보이게
   slidesToScroll: 1, // 1장씩 뒤로 넘어가게
-  centerMode: true,
   centerPadding: '0px', // 0px 하면 슬라이드 끝쪽 이미지가 안잘림
 };
 
@@ -91,14 +96,85 @@ const CardBox = styled.div`
   cursor: pointer;
 `;
 
+const CardImgBox = styled.div`
+  position: relative;
+  width: 224px;
+  height: 319px;
+  ::before {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 319px;
+    background-image: linear-gradient(
+      to bottom,
+      rgba(0, 0, 0, 0),
+      rgba(0, 0, 0, 0.09) 35%,
+      rgba(0, 0, 0, 0.85)
+    );
+    content: '';
+  }
+  &:hover strong {
+    transition: all 0.3s ease-out;
+    display: none;
+  }
+  &:hover::before {
+    transition: all 0.3s ease-out;
+    background: rgba(0, 0, 0, 0.6);
+  }
+  ::after {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin-top: -25px;
+    margin-left: -75px;
+    content: '상세보기';
+    text-align: center;
+    line-height: 50px;
+    width: 150px;
+    height: 50px;
+    background: #fff;
+    color: #333;
+    font-weight: bold;
+    border-radius: 5px;
+    z-index: -1;
+    opacity: 0;
+  }
+  &:hover::after {
+    transition: all 0.3 ease-in-out;
+    opacity: 0.9;
+    z-index: 1;
+  }
+`;
+
+const CardRanking = styled.strong`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  font-size: 70px;
+  color: #fff;
+  padding: 0 10px;
+  line-height: 1;
+  font-style: italic;
+`;
+
 const CardImg = styled.img`
-  width: 285px;
-  height: 360px;
+  width: 224px;
+  height: 319px;
+`;
+
+const CardTitle = styled.h3`
+  font-size: 20px;
+  font-weight: bold;
+  width: 224px;
+  padding: 20px 0;
+  line-height: 1;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
 `;
 
 const CardText = styled.p`
-  padding: 20px;
-  font-size: 20px;
-  font-weight: bolder;
-  text-align: center;
+  font-size: 15px;
+  color: #444;
 `;
--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -3,34 +3,60 @@ import Slider from '../../components/slider/Slider';
 const HomePage = () => {
   const sliderItem = [
     {
-      name: '1',
+      ranking: '1',
+      title: '닥터 스트레인지-대혼돈의 멀티버스',
+      releaseDate: '2022.05.04',
       image:
         'https://img.cgv.co.kr/Movie/Thumbnail/Poster/000085/85715/85715_320.jpg',
     },
     {
-      name: '2',
+      ranking: '2',
+      title: '신비한 동물들과 덤블도어의 비밀',
+      releaseDate: '2022.05.04',
       image:
         'https://img.cgv.co.kr/Movie/Thumbnail/Poster/000085/85712/85712_320.jpg',
     },
     {
-      name: '3',
+      ranking: '3',
+      title: '세븐틴 파워 오브 러브 - 더 무비',
+      releaseDate: '2022.05.04',
       image:
         'https://img.cgv.co.kr/Movie/Thumbnail/Poster/000085/85791/85791_320.jpg',
     },
     {
-      name: '4',
+      ranking: '4',
+      title: '앵커',
+      releaseDate: '2022.05.04',
       image:
         'https://img.cgv.co.kr/Movie/Thumbnail/Poster/000085/85772/85772_320.jpg',
     },
     {
-      name: '5',
+      ranking: '5',
+      title: '공기 살인',
+      releaseDate: '2022.05.04',
       image:
         'https://img.cgv.co.kr/Movie/Thumbnail/Poster/000085/85780/85780_320.jpg',
     },
     {
-      name: '6',
+      ranking: '6',
+      title: '로스트 시티',
+      releaseDate: '2022.05.04',
       image:
         'https://img.cgv.co.kr/Movie/Thumbnail/Poster/000085/85787/85787_320.jpg',
+    },
+    {
+      ranking: '7',
+      title: '니 부모 얼굴이 보고 싶다',
+      releaseDate: '2022.05.04',
+      image:
+        'https://img.cgv.co.kr/Movie/Thumbnail/Poster/000085/85821/85821_320.jpg',
+    },
+    {
+      ranking: '8',
+      title: '봄날',
+      releaseDate: '2022.05.04',
+      image:
+        'https://img.cgv.co.kr/Movie/Thumbnail/Poster/000085/85799/85799_320.jpg',
     },
   ];
   return (
